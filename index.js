024a4af5ad4fe5f4131f736b1a5ac0a293d88a6b
Reuben Remy <reubenremy@gmail.com>
2019-12-11 14:42:30 -0500

    Refactored for simplicity

--- index.js
+++ index.js
@@ -12,14 +12,10 @@ readlineInterface.question(`Give me a number between 1 & 10? : `, function(numbe
 
 function multiplyByTwo(number) {
     let response = "";
-    if (number > 10) {
-        response = "Number is too large!! Choose a number less than 10"
-    }
-    if (number < 1) {
-        response = "Number is too low!! Choose a number greater than 1!"
+    if (number >= 10 || number <= 1) {
+        response = "Number is out of range!! Choose another number"
     }
     if (number < 10 && number > 1) {
         response = number * 2;
-    }
     return response;
 }
