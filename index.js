187d610c0ce779909c09e074b4a24b33678116c0
Reuben Remy <reubenremy@gmail.com>
2019-12-11 13:36:28 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const readline =
+require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question(`What's your name? :`, function(name) {
+    console.log(`Hi ${name}!`);
+    readline.close();
+})
