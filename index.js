d36fafd94e8003872e99ce3c7d1c8e7ec2b8f9ea
Reuben Remy <reubenremy@gmail.com>
2020-12-24 20:12:29 -0500

    .

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const readlineInterface = readline.createInterface({
     output: process.stdout
 })
 
-readlineInterface.question(`Give me a number between 1 & 10? : `, function(number) {
-    console.log(multiplyByTwo(number));
+readlineInterface.question(`Enter Transaction: `, (transaction) => {
+    console.log('you typed', transaction)
     readlineInterface.close();
 })
 
-function multiplyByTwo(number) {
-    let response = "";
-    if (number >= 10 || number <= 1) {
-        response = "Number is out of range!! Choose another number"
-    }
-    if (number < 10 && number > 1) {
-        response = number * 2;
-    return response;
-}
+// const multiplyByTwo = number => {
+//     let response = "";
+//     if (number >= 10 || number <= 1) {
+//         response = "Number is out of range!! Choose another number"
+//     }
+//     if (number < 10 && number > 1) {
+//         response = number * 2;
+//     return response;
+//     }
+// }
